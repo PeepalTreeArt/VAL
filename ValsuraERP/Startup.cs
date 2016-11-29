669c8b6085d1b5631feee586df00507c35819a7f
Pawan <pawanyadav2006@gmail.com>
2016-11-29 15:26:19 +0530

    Add project files.

---
+++ ValsuraERP/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ValsuraERP.Startup))]
+namespace ValsuraERP
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
